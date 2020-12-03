0e0175cebb4989b40a8777d3ee0a0374feee8a38
milansteenwinkel <milan.steenwinkel@incentro.com>
2020-12-03 09:54:53 +0100

    Add part 1 of day 3

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,15 +1,47 @@
-import { test, readInput } from "../utils/index"
+import {test, readInput} from "../utils/index"
 
 const prepareInput = (rawInput: string) => rawInput
 
 const input = prepareInput(readInput())
+const rows = input.split(/\r?\n/).filter(i => i);
 
 const goA = (input) => {
-  return
+    return doStep(rows, 0, 0, 0, 3, 1);
 }
 
+function doStep(rows, currentX, currentY, totalTrees, xStep, yStep): number {
+    let x = currentX + xStep;
+    let y = currentY + yStep;
+    let foundTree = false;
+    if (y > rows.length - 1) {
+        console.log(totalTrees);
+        return totalTrees;
+    }
+
+    if (rows[y].length >= x) {
+        foundTree = rows[y].charAt(x) == '#';
+        if (foundTree) {
+            totalTrees++;
+        }
+        return doStep(rows, x, y, totalTrees, xStep, yStep);
+    } else {
+        const calibratedX = ((x) % rows[y].length);
+        foundTree = rows[y].charAt(calibratedX) == '#';
+        if (foundTree) {
+            totalTrees++;
+        }
+        return doStep(rows, x, y, totalTrees, xStep, yStep);
+    }
+
+}
+
+// goes wrong, still need to do.... after work it is
 const goB = (input) => {
-  return
+    return doStep(rows, 0, 0, 0, 1, 1)
+        * doStep(rows, 0, 0, 0, 3, 1)
+        * doStep(rows, 0, 0, 0, 5, 1)
+        * doStep(rows, 0, 0, 0, 7, 1)
+        * doStep(rows, 0, 0, 0, 1, 2);
 }
 
 /* Tests */
